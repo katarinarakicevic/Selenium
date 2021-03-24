bdf0e7e8694a5eade4706dee31a91f0daf71cca1
Katarina Rakicevic <53061439+katarinarakicevic@users.noreply.github.com>
2021-03-24 21:24:35 +0100

    Create salesForce.java

---
+++ src/salesForce.java
@@ -0,0 +1,22 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class salesForce {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//chromedriver.exe
+		System.setProperty( "webdriver.chrome.driver","C:\\Users\\Katarina\\Downloads\\chromedriver.exe");
+	
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://login.salesforce.com/");
+		driver.findElement(By.id("username")).sendKeys("test");
+		driver.findElement(By.name("pw")).sendKeys("123456");
+		/*driver.findElement(By.className("button r4 wide primary")).click();//error
+		  */
+		driver.findElement(By.name("Login")).click();
+		
+	}
+
+}
